9a306da5cfb0f0e6f5e84b9a59cb35983a841148
Don <hamalainen.don@gmail.com>
2024-02-07 23:20:31 +0200

    gsap installed

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,60 @@
-import Link from "next/link";
-import Image from "next/image";
-import DEMO from "public/images/demo2.jpg";
+"use client";
+
+import { useRef, useEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
 export default function Home() {
+  const headerText = useRef(null);
+  const headerText2 = useRef(null);
+  const slider = useRef(null);
+  let xPercent = 0;
+  let direction = -1;
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.to(slider.current, {
+      scrollTrigger: {
+        trigger: document.documentElement,
+        scrub: 0.25,
+        start: 0,
+        end: window.innerHeight,
+        onUpdate: (e) => (direction = e.direction * -1),
+      },
+      x: "-500px",
+    });
+    requestAnimationFrame(animate);
+  }, []);
+
+  const animate = () => {
+    if (xPercent < -100) {
+      xPercent = 0;
+    } else if (xPercent > 0) {
+      xPercent = -100;
+    }
+    gsap.set(headerText.current, { xPercent: xPercent });
+    gsap.set(headerText2.current, { xPercent: xPercent });
+    requestAnimationFrame(animate);
+    xPercent += 0.1 * direction;
+  };
+
   return (
-    <main className="px-5">
+    <main className="px-5 overflow-hidden">
       <div className="h-full w-full md:h-[calc(100vh-40vh)] ">
-       
+        <div className="flex items-center" ref={slider}>
+          <h1
+            ref={headerText}
+            className="text-[400px] dark:text-primary-light text-primary-dark whitespace-nowrap"
+          >
+            Aidosti raikasta.
+          </h1>
+          <h1
+            ref={headerText2}
+            className="text-[400px] dark:text-primary-light text-primary-dark whitespace-nowrap"
+          >
+            Aidosti raikasta.
+          </h1>
+        </div>
       </div>
     </main>
   );
