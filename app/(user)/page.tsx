d0bde96f6df163c625ce49dda622e953f300061c
Don Hämäläinen <118666926+donhamalainen@users.noreply.github.com>
2024-03-02 12:00:03 +0200

    update

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 import { homeText } from "@/libs/homeText";
 import { useState, useEffect, useRef } from "react";
 import { gsap } from "gsap";
-import DefaultImage from "@/public/images/reetta.jpg";
+import DefaultImage from "@/public/images/headerPic.jpg";
+import ReettaImage from "@/public/images/reetta.png";
 export default function Home() {
   // Attributes
   const [referenceIndex, setReferenceIndex] = useState(0);
@@ -28,36 +29,34 @@ export default function Home() {
   // RETURN
   return (
     <main className="px-5 pt-20 relative overflow-hidden">
-      <div
-        aria-hidden
-        className="absolute -right-20 top-0 rounded-full h-1/2 w-1/2 bg-[#F4538A] blur-[clamp(100px,15vw,380px)]"
-      />
-      <section className="w-full relative">
+      <section className="w-full relative mb-20">
         {/* HEADER */}
         <div className="relative w-full">
           <Image
             src={DefaultImage}
             alt="Reetta"
             priority
+            loading="eager"
             className="w-full h-[500px] object-cover rounded-xl z-10"
           />
           <div className="absolute h-full w-full bg-secondary-darkText/40 top-0 left-0 right-0 bottom-0 rounded-xl" />
-          <div className="grid grid-cols-3 gap-10 absolute left-10 bottom-5 right-10">
-            <div className="col-span-2">
-              <span>
-                Olen Reetta Salmela, sosiaalinen ja puhelias parikymppinen
-                journalistiopiskelija Oulusta. Lapsuuteni on ollut täynnä
-                erilaisia pallopelejä, jotka opettivat minulle tiimityöskentelyn
-                arvon. Innostun helposti ja pyrin toimimaan tehokkaasti asioita
-                edistääkseni.
-              </span>
-            </div>
-            <div className="xl:text-9xl text-right text-primary-secondBackground mix-blend-soft-light">
-              <span>Aidosti raikasta.</span>
-            </div>
+          <div className="absolute bottom-5 right-10 items-end">
+            <span className="xl:text-9xl text-right text-primary-secondBackground mix-blend-soft-light items-end font-author">
+              Aidosti raikasta.
+            </span>
           </div>
         </div>
-        {/* HEADER */}
+      </section>
+      {/* HEADER */}
+      <section className="h-screen">
+        <div className="retlative">
+          <Image
+            src={ReettaImage}
+            alt="Reetta"
+            className="h-[450px] w-max object-contain bg-primary-secondBackground rounded-full"
+          />
+          <h1 className="col-span-2"></h1>
+        </div>
       </section>
     </main>
   );
--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function footer() {
-  return <footer className="h-screen mt-20">footer</footer>;
+  return (
+    <footer className="bg-primary-secondBackground mt-20 h-[200px]">
+      <div className="flex">
+        <span>LinkedIn</span>
+      </div>
+    </footer>
+  );
 }
