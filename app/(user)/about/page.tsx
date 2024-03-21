5f2b8aad09bb0bef5e274e75bd743b0dbe1c823c
Don Hämäläinen <118666926+donhamalainen@users.noreply.github.com>
2024-03-21 22:23:47 +0200

    Yhteistiedot

--- app/(user)/about/page.tsx
+++ app/(user)/about/page.tsx
@@ -2,6 +2,7 @@
 
 import React, { useEffect, useState } from "react";
 import getFinnishTime from "@/hooks/getTime";
+import FormComponent from "@/components/FormComponent";
 export default function about() {
   const [time, setTime] = useState("");
   useEffect(() => {
@@ -14,10 +15,27 @@ export default function about() {
   return (
     <section className="px-10">
       <div className="md:grid md:grid-cols-2 relative">
-        <h2 className="text-[11.5rem] leading-none font-bold text-secondary-darkText">
+        <h2 className="text-[clamp(48px,5vw,120px)] leading-none font-bold text-secondary-darkText">
           Yhteistiedot.
         </h2>
+        <p className="py-4 font-authorItalic">
+          Olen iloinen, että olette kiinnostunut ottamaan yhteyttä. Olipa
+          kyseessä sitten tarve freelance-yhteistyöhön, mahdollisuus yhteisiin
+          projekteihin tai yksinkertaisesti keskustelu alaan liittyvistä
+          ideoista ja näkemyksistä, otan mielelläni vastaan viestinne.
+          <br />
+          Olen vakuuttunut siitä, että avoin ja rakentava kommunikointi on avain
+          menestyksekkääseen yhteistyöhön. Olen sitoutunut tarjoamaan
+          korkealaatuista työtä ja täyttämään asetetut tavoitteet aikataulussa.
+          Arvostan mahdollisuutta kuulla lisää tarpeistanne ja mahdollisuuksista
+          yhteistyöhön.
+          <br />
+          Kiitos, että harkitsette yhteydenottoa. Odotan innolla mahdollisuutta
+          keskustella kanssanne ja löytää tapoja, joilla voimme tukea toinen
+          toisiamme.
+        </p>
       </div>
+      <FormComponent />
     </section>
   );
 }
---
+++ components/FormComponent.tsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import nodemailer from "nodemailer";
+
+export default function FormComponent() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    company: "",
+    phone: "",
+    message: "",
+  });
+
+  return (
+    <form>
+      <div className="relative mb-4">
+        <label htmlFor="email" className="block font-bold mb-1">
+          Nimi <span className="text-red-500">*</span>
+        </label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder=" "
+          className="border border-gray-300 rounded-md px-3 py-2 w-full"
+        />
+      </div>
+
+      <div className="mb-4">
+        <label htmlFor="email" className="block font-bold mb-1">
+          Sähköposti <span className="text-red-500">*</span>
+        </label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          required
+          className="border border-gray-300 rounded-md px-3 py-2 w-full"
+        />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="company" className="block font-bold mb-1">
+          Yritys
+        </label>
+        <input
+          type="text"
+          id="company"
+          name="company"
+          className="border border-gray-300 rounded-md px-3 py-2 w-full"
+        />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="phone" className="block font-bold mb-1">
+          Puhelinnumero
+        </label>
+        <input
+          type="tel"
+          id="phone"
+          name="phone"
+          className="border border-gray-300 rounded-md px-3 py-2 w-full"
+        />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="message" className="block font-bold mb-1">
+          Viesti <span className="text-red-500">*</span>
+        </label>
+        <textarea
+          id="message"
+          name="message"
+          required
+          className="border border-gray-300 rounded-md px-3 py-2 w-full"
+        ></textarea>
+      </div>
+      <button
+        type="submit"
+        className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+      >
+        Lähetä
+      </button>
+    </form>
+  );
+}
